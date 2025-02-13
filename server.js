96d8e6668435abab898da0737c4642570d75ff82
Aryam2121 <aryamangupta2121@gmail.com>
2025-02-13 15:31:19 +0530

    cors solve

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
